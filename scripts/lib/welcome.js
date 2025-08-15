fa144ed59da0c090e0e8476dfd6f4464030c167a
Alex Emch <77809775+Emchtastic@users.noreply.github.com>
2025-08-15 12:54:37 -0600

    Remove theripper93 video reference from welcome message

--- scripts/lib/welcome.js
+++ scripts/lib/welcome.js
@@ -5,13 +5,10 @@ const SHOWN_KEY = "chat-welcome-message-shown";
 export function showWelcome() {
     if (!game.user.isGM) return;
     const module = game.modules.get(MODULE_ID);
-    const VIDEO_ID = "2Iq_so_GsLA";
-    const EMBEDDED_VIDEO = `<iframe width="100%" height="auto" src="https://www.youtube.com/embed/${VIDEO_ID}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
     const WIKI_URL = `https://github.com/Emchtastic/tides-of-battle`;
     const MESSAGE = `<h1>${module.title}</h1>
     <p>by <a href="https://github.com/Emchtastic">Emchtastic</a></p>
     <p><strong>New to ${module.title}? Visit the <a href="${WIKI_URL}">GitHub Repository</a> for documentation and resources.</strong></p>
-    ${VIDEO_ID ? EMBEDDED_VIDEO : ""}
     <p>This module builds upon the excellent foundation of <a href="https://github.com/theripper93/combat-tracker-dock">Carousel Combat Tracker</a> by theripper93.</p>
     <p><strong>Explore project resources:</strong></p> <ul>
     <li><a href="https://github.com/Emchtastic/tides-of-battle">GitHub Repository</a></li>
