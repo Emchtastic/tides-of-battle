bb5b771528fbdebd18395b2ccb27d286190b85c6
Alex Emch <77809775+Emchtastic@users.noreply.github.com>
2025-08-15 12:49:53 -0600

    Update welcome message authorship and links to Emchtastic

--- scripts/lib/welcome.js
+++ scripts/lib/welcome.js
@@ -7,17 +7,16 @@ export function showWelcome() {
     const module = game.modules.get(MODULE_ID);
     const VIDEO_ID = "2Iq_so_GsLA";
     const EMBEDDED_VIDEO = `<iframe width="100%" height="auto" src="https://www.youtube.com/embed/${VIDEO_ID}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-    const WIKI_URL = module.protected ? `https://wiki.theripper93.com/premium/${MODULE_ID}` : `https://wiki.theripper93.com/free/${MODULE_ID}`;
+    const WIKI_URL = `https://github.com/Emchtastic/tides-of-battle`;
     const MESSAGE = `<h1>${module.title}</h1>
-    <p>by <a href="https://theripper93.com">theripper93</a></p>
-    <p><strong>New to ${module.title}? Visit the <a href="${WIKI_URL}">Wiki</a> for a quickstart guide and resources.</strong></p>
+    <p>by <a href="https://github.com/Emchtastic">Emchtastic</a></p>
+    <p><strong>New to ${module.title}? Visit the <a href="${WIKI_URL}">GitHub Repository</a> for documentation and resources.</strong></p>
     ${VIDEO_ID ? EMBEDDED_VIDEO : ""}
-    <p>Special thanks to all my <a href="https://www.patreon.com/theripper93">Patreons</a> for making ongoing updates and development possible. Supporting gives you access to <strong>30+ premium modules</strong> and <strong>priority support</strong>.</p>
-    <p><strong>Explore all resources and modules:</strong></p> <ul>
-    <li><a href="https://theripper93.com/">Check out all my free and premium modules</a></li>
-    <li><a href="${WIKI_URL}">${module.title} Wiki</a></li>
-    <li><a href="https://www.patreon.com/theripper93">Support on Patreon</a></li>
-    <li><a href="https://discord.theripper93.com/">Discord Channel</a></li>
+    <p>This module builds upon the excellent foundation of <a href="https://github.com/theripper93/combat-tracker-dock">Carousel Combat Tracker</a> by theripper93.</p>
+    <p><strong>Explore project resources:</strong></p> <ul>
+    <li><a href="https://github.com/Emchtastic/tides-of-battle">GitHub Repository</a></li>
+    <li><a href="https://github.com/Emchtastic/tides-of-battle/issues">Report Issues</a></li>
+    <li><a href="https://github.com/Emchtastic">More Projects by Emchtastic</a></li>
     </ul>`;
 
 
